5b454e967973701d0dca87e9ffa039fdb09013c5
Dewsy <dewsyy@gmail.com>
2019-07-31 17:18:41 +0200

    basic input_component added

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:angular_app/src/gradient_service.dart';
 import 'package:angular_app/src/cell_component.dart';
+import 'package:angular_app/src/input_component.dart';
 import 'src/gradient_component.dart';
 
 @Component(
     selector: 'my-app',
     templateUrl: "app_component.html",
-    directives: [coreDirectives, CellComponent, GradientComponent],
+    directives: [coreDirectives, CellComponent, GradientComponent, InputComponent],
     providers: [ClassProvider(GradientService)])
-class AppComponent {
-
-}
+class AppComponent {}
