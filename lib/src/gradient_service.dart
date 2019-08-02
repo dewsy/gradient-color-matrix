d00d5fdee2c40dc178464eb1d515b35dbdc5aa62
Dewsy <dewsyy@gmail.com>
2019-08-02 16:04:57 +0200

    renamed some variables for better readability

--- lib/src/gradient_service.dart
+++ lib/src/gradient_service.dart
@@ -19,14 +19,15 @@ class GradientService {
   }
 
   List<List<Cell>> createGradient(int n) {
-    List<int> horC = [64];
+    List<int> horizontalCharCodes = [64];
     List<List<Cell>> gradient = List(n + 1);
     gradient[0] = _createVerticalMarkers(n);
     List<String> colorValues = _getColors(n);
     for (int i = 0; i < n; i++) {
       List<Cell> row = List(n + 1);
-      horC = _createHorizontalMarkerCodes(horC);
-      row[0] = Cell.withMessage(String.fromCharCodes(horC.reversed.toList()));
+      horizontalCharCodes = _createHorizontalMarkerCodes(horizontalCharCodes);
+      row[0] = Cell.withMessage(
+          String.fromCharCodes(horizontalCharCodes.reversed.toList()));
       for (int k = 0; k < n; k++) {
         row[k + 1] = Cell.withColor(colorValues[k + i]);
       }
@@ -44,19 +45,19 @@ class GradientService {
     return marker;
   }
 
-  List<int> _createHorizontalMarkerCodes(List<int> horizontalCharCodes) {
-    for (int i = 0; i < horizontalCharCodes.length; i++) {
-      if (horizontalCharCodes[i] < 90) {
-        horizontalCharCodes[i]++;
+  List<int> _createHorizontalMarkerCodes(List<int> horC) {
+    for (int i = 0; i < horC.length; i++) {
+      if (horC[i] < 90) {
+        horC[i]++;
         break;
       } else {
-        horizontalCharCodes[i] = 65;
-        if (i + 1 == horizontalCharCodes.length) {
-          horizontalCharCodes.add(65);
+        horC[i] = 65;
+        if (i + 1 == horC.length) {
+          horC.add(65);
           break; //the add(65) makes it run one extra time without this
         }
       }
     }
-    return horizontalCharCodes;
+    return horC;
   }
 }
